a16f2004faef1ce00bbd842b3492e8638d5c8322
DougaXD <143052043+DougaXD@users.noreply.github.com>
2024-06-23 15:02:38 -0300

    update scripts/script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,11 @@
+function mudouTamanho(){
+  if(window.innerWidth >= 768px){
+    itens.style.display = 'none';
+  }else{
+    itens.style.display = 'none';
+  }
+}
+
 function clickMenu(){
   var itens = document.getElementById("itens");
   if(itens.style.display == 'block'){
